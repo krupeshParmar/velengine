473bb59004666f1b4b4e4f96af8416a3dab58d58
Krupesh Parmar <parmarkrupesh786@gmail.com>
2023-02-20 20:58:24 -0500

    PhysicsProject1

--- Sandbox/src/VellApp.cpp
+++ Sandbox/src/VellApp.cpp
@@ -6,6 +6,42 @@
 #include "temp/PhysicsProject1.h"
 #include "Example.h"
 
+/*
+*	Student name:	Krupesh Ashok Parmar
+*	Student number:	1124374
+*	Student mail:	k_parmar180076@fanshaweonline.ca
+*	Program name:	GAME DEVELOPMENT ADVANCED PROGRAMMING
+*	Course name:	Physics & Simulation II
+*
+*				Project 1: Rigidbodies
+*
+*	a) How to build this project:
+*
+*		- Clone the provided git link
+*
+*		- Inside the root folder, double click on "genproj.bat" to generate the solution file
+*
+*		- Sandbox project should be the main project, so just hit "F5" or "Ctrl + F5"
+*
+*
+*	b) How to run this project:
+*		- You can run project only from Visual Studio using key "F5" or "Ctrl + F5"
+*
+*	c) User inputs & info:
+*		- Left - Right: To rotate the camera around the center
+*		- Up - Down: To zoom in and out of the scene
+*		- WASD: To move the selected ball relative to the camera
+*		- 1 to 5: To select balls from one to five
+*	
+*		Code locations:	
+*
+*		- iPhysics solution
+*		- VelPhysics solution
+*		- PhysicsProject1.h and .cpp files from the Sandbox solution
+* 
+* 
+*/
+
 class VelApp : public vel::Application
 {
 public:
